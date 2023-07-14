674755f3668ba94ec4cbc565ba7c3482c0725f15
Huy Phan <huydev@Huys-MacBook-Pro.local>
2023-07-14 21:03:36 +0700

    fix(button): fix UI button

--- shared/see-more-button.tsx
+++ shared/see-more-button.tsx
@@ -20,7 +20,7 @@ const SeeMoreButton: React.FC<Props> = ({ text,loadMore,loading }) => {
           className="h-14 w-[18.5rem] bg-black text-light-100 font-light rounded mx-auto flex items-center justify-center gap-2"
         >
           
-          {loading && <LoadingSpinner color="white" size={20} className="mx-0 pt-1 pb-0"/>}
+          {loading && <LoadingSpinner color="white" size={20} className="!mx-0 pt-1 pb-0"/>}
           {text}
         </button>
       </div>
